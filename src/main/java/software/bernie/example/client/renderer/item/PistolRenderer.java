df97e8e67c41131d8b7bdfb142c08178fb0795a5
Kam <KamronVaswani@gmail.com>
2025-07-28 20:31:06 -0400

    Refine pistol renderer transforms

--- src/main/java/software/bernie/example/client/renderer/item/PistolRenderer.java
+++ src/main/java/software/bernie/example/client/renderer/item/PistolRenderer.java
@@ -3,14 +3,6 @@ package software.bernie.example.client.renderer.item;
 import software.bernie.example.client.model.item.PistolModel;
 import software.bernie.example.item.PistolItem;
 import software.bernie.geckolib3.renderers.geo.GeoItemRenderer;
-import software.bernie.geckolib3.geo.render.built.GeoModel;
-import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
-import software.bernie.geckolib3.core.util.Color;
-
-import net.minecraft.client.Minecraft;
-import net.geckominecraft.client.renderer.GlStateManager;
-import java.util.Collections;
-
 import net.minecraftforge.client.IItemRenderer.ItemRenderType;
 import net.minecraft.item.ItemStack;
 import org.lwjgl.opengl.GL11;
@@ -31,12 +23,10 @@ public class PistolRenderer extends GeoItemRenderer<PistolItem> {
             switch (type) {
                 case EQUIPPED_FIRST_PERSON:
                     GL11.glTranslatef(-9.5f / 16f, -6.75f / 16f, -8.75f / 16f);
-                    GL11.glRotatef(-90, 0, 1, 0);
                     break;
                 case EQUIPPED:
                     GL11.glTranslatef(0, -1f / 16f, -0.25f / 16f);
                     GL11.glScalef(0.3f, 0.3f, 0.3f);
-                    GL11.glRotatef(-90, 0, 1, 0);
                     break;
                 case INVENTORY:
                     GL11.glTranslatef(-0.25f / 16f, -1.25f / 16f, 0);
@@ -47,42 +37,15 @@ public class PistolRenderer extends GeoItemRenderer<PistolItem> {
                     break;
                 case ENTITY:
                     GL11.glScalef(0.5f, 0.5f, 0.5f);
-                    GL11.glRotatef(-90, 0, 1, 0);
                     break;
                 default:
                     break;
             }
 
-            // delegate to base renderer (without additional transforms)
+            // delegate to base renderer applying default rotation
             super.renderItem(type, stack, data);
         } finally {
             GL11.glPopMatrix();
-        }
-    }
-
-    @Override
-    public void render(PistolItem animatable, ItemStack stack) {
-        // Override to avoid the +90� Y rotation from the parent implementation
-        this.currentItemStack = stack;
-        GeoModel model = this.getGeoModelProvider().getModel(this.getGeoModelProvider().getModelLocation(animatable));
-        AnimationEvent<PistolItem> itemEvent = new AnimationEvent<>(animatable, 0, 0,
-            Minecraft.getMinecraft().timer.renderPartialTicks, false, Collections.singletonList(stack));
-        this.getGeoModelProvider().setLivingAnimations(animatable, this.getUniqueID(animatable), itemEvent);
-
-        GlStateManager.pushMatrix();
-        try {
-            GlStateManager.translate(0, 0.01f, 0);
-            GlStateManager.translate(0.5f, 0.5f, 0.5f);
-
-            Minecraft.getMinecraft().renderEngine.bindTexture(getTextureLocation(animatable));
-            Color renderColor = getRenderColor(animatable, 0f);
-            render(model, animatable, 0,
-                (float) renderColor.getRed() / 255f,
-                (float) renderColor.getGreen() / 255f,
-                (float) renderColor.getBlue() / 255f,
-                (float) renderColor.getAlpha() / 255f);
-        } finally {
-            GlStateManager.popMatrix();
         }
     }
 }
